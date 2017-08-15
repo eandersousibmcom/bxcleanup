4712edc60a3ad5b36d9ede3b87711181e7f26618
Erik D. Anderson <eanderso@us.ibm.com>
2017-08-15 16:28:20 -0400

    Last version prior to actually stopping apps

--- bxcleanup.sh
+++ bxcleanup.sh
@@ -25,8 +25,22 @@ for i in $orgs
 
     for j in $spaces
       {
-            echo "Targeting space: "$j".";
+            echo "Getting running apps in space: "$j".";
             bx target -s $j;
+
+            #Get the list of all the active apps
+            apps=`bx app list | grep started | awk '{print $1}'`
+
+            #Check for apps in the whitelist_apps
+            for k in $apps
+              {
+                if grep -Fxq $k whitelist_apps
+                then
+                    echo $k" is in the whitelist."
+                else
+                    echo $k" is NOT in the whitelist. Stopping..."
+                fi
+              }
 
       }
 
